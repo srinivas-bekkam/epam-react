dfe19b8dd45d6b803e8dbce246474063d5bbafbd
Srinivas Bekkam <srinivasbek@gmail.com>
2025-01-07 16:54:17 +0530

    added cards

--- host/src/ShoppingList.jsx
+++ host/src/ShoppingList.jsx
@@ -7,6 +7,11 @@ import Svg from '../src/react-svg.svg'
 import { useGetAlbumsQuery } from "./Store/AlbumsQuery"
 import { useGetShoppingListQuery } from "./Store/ShoppingCartQuery"
 import { Button } from "@mui/material"
+import ProductCard from "./CustomInputs/ProductCard"
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid2';
+
 export default function ShoppingList(){
      let [page, setPage]=useState(1)
      const pageSize=10;
@@ -34,14 +39,21 @@ export default function ShoppingList(){
     },[page,isFetching])
     
     return (
-    <div>
+    <div container spacing={2}>
+
         {/* <Button variant="contained"> Button</Button> */}
         {isFetching && `Loading...`}
+        <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={4}>
+   
         {data && data.map((alb)=>{
-            return (<div key={alb.id}>
-                <h3>{alb.title}</h3>
-            </div>)
+            return (<Grid key={alb.id} className="gl" size={{ xs: 6, md: 4, lg:3 }}>
+                {/* <h3>{alb.title}</h3> */}
+                <ProductCard data={alb}></ProductCard>
+            </Grid>)
         })}
+          </Grid>
+          </Box>
         {/* <Counter2/>
         <Header/> */}
     </div>
