36d28cef0e7c47de382108e46e0f2d67015fa7a2
Srinivas Bekkam <srinivasbek@gmail.com>
2025-01-16 11:50:55 +0530

    Revert "added testing"
    This reverts commit 1508b9470041b08a29bc9ecaf1c671e650e9f602.

--- host/setUpTests.js
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom";
-
-import { server } from "./src/mocks/server";
-import {} from "./src";
-// Start and close the server for tests
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
--- host/src/reduxstore/AlbumsQuery.jsx
+++ host/src/reduxstore/AlbumsQuery.jsx
@@ -1,25 +1,24 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-export const AlbumsQuery = createApi({
-	reducerPath: "AlbumsQuery",
-	baseQuery: fetchBaseQuery({
-		baseUrl: "https://jsonplaceholder.typicode.com/",
-	}),
-	endpoints: (builder) => ({
-		getAlbums: builder.query({
-			query: (page) => `/albums?_pages=${page}&_limit=10`,
-			// Only have one cache entry because the arg always maps to one string
-			serializeQueryArgs: ({ endpointName }) => {
-				return endpointName;
-			},
-			// Always merge incoming data to the cache entry
-			merge: (currentCache, newItems) => {
-				currentCache.push(...newItems);
-			},
-			// Refetch when the page arg changes
-			forceRefetch({ currentArg, previousArg }) {
-				return currentArg !== previousArg;
-			},
-		}),
-	}),
-});
-export const { useGetAlbumsQuery } = AlbumsQuery;
+import { createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+export const  AlbumsQuery = createApi({
+    reducerPath:'AlbumsQuery',
+    baseQuery:fetchBaseQuery({baseUrl:"https://jsonplaceholder.typicode.com/"}),
+    endpoints:(builder)=>({
+        getAlbums:builder.query({
+            query:(page)=>`/albums?_pages=${page}&_limit=10`,
+             // Only have one cache entry because the arg always maps to one string
+            serializeQueryArgs: ({ endpointName }) => {
+                return endpointName
+            },
+            // Always merge incoming data to the cache entry
+            merge: (currentCache, newItems) => {
+                currentCache.push(...newItems)
+            },
+            // Refetch when the page arg changes
+            forceRefetch({ currentArg, previousArg }) {
+                return currentArg !== previousArg
+            },
+        })
+
+    })
+})
+export const {useGetAlbumsQuery}=AlbumsQuery;
